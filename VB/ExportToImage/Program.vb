460e900f27947c434f8f4c8d9ef97223fa949074
DevExpressExampleBot <fakeEmail>
2019-04-09 11:19:40 +0300

    Source auto update [skip ci]

--- VB/ExportToImage/Program.vb
+++ VB/ExportToImage/Program.vb
@@ -2,32 +2,32 @@
 Imports System.Drawing
 
 Namespace ExportToImage
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
 
-            Dim largestEdgeLength As Integer = 1000
-            Dim pageNumbers() As Integer = { 1, 3, 5 }
+			Dim largestEdgeLength As Integer = 1000
+			Dim pageNumbers() As Integer = { 1, 3, 5 }
 
-            ' Create a PDF Document Processor.
-            Using processor As New PdfDocumentProcessor()
+			' Create a PDF Document Processor.
+			Using processor As New PdfDocumentProcessor()
 
-                ' Load a document. 
-                processor.LoadDocument("..\..\Document.pdf")
+				' Load a document. 
+				processor.LoadDocument("..\..\Document.pdf")
 
-                ' Export pages to a multi-page tiff image.
-                processor.CreateTiff("..\..\Image.tiff", largestEdgeLength, pageNumbers)
+				' Export pages to a multi-page tiff image.
+				processor.CreateTiff("..\..\Image.tiff", largestEdgeLength, pageNumbers)
 
-                For i As Integer = 1 To processor.Document.Pages.Count
+				For i As Integer = 1 To processor.Document.Pages.Count
 
-                    ' Export pages to bitmaps.
-                    Dim image As Bitmap = processor.CreateBitmap(i, largestEdgeLength)
+					' Export pages to bitmaps.
+					Dim image As Bitmap = processor.CreateBitmap(i, largestEdgeLength)
 
-                    ' Save the bitmaps.
-                    image.Save("..\..\MyBitmap" & i & ".bmp")
-                Next i
-            End Using
-        End Sub
-    End Class
+					' Save the bitmaps.
+					image.Save("..\..\MyBitmap" & i & ".bmp")
+				Next i
+			End Using
+		End Sub
+	End Class
 End Namespace
 
 
