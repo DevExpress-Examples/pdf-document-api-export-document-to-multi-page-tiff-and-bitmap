b8e0279fcba834ed99103dcf5aa8c19c11142c23
guemjalee <polina.fedorova@devexpress.com>
2024-12-02 17:39:26 +0100

    add svg export

--- CS/ExportToImage/Program.cs
+++ CS/ExportToImage/Program.cs
@@ -1,33 +1,46 @@
-﻿using DevExpress.Pdf;
-using System.Drawing;
+﻿using DevExpress.Drawing;
+using DevExpress.Pdf;
 
-namespace ExportToImage {
-    class Program {
-        static void Main(string[] args) {
+namespace ExportToImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
             int largestEdgeLength = 1000;
             int[] pageNumbers = new int[] { 1, 3, 5 };
 
             // Create a PDF Document Processor.
-            using (PdfDocumentProcessor processor = new PdfDocumentProcessor()) {
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor())
+            {
 
                 // Load a document. 
                 processor.LoadDocument("..\\..\\Document.pdf");
 
                 // Export pages to a multi-page tiff image.
-                processor.CreateTiff("..\\..\\Image.tiff", largestEdgeLength, pageNumbers);
+                processor.CreateTiff("..\\..\\Image.tiff", pageNumbers, 96);
 
-                for (int i = 1; i <= processor.Document.Pages.Count; i++) {
+                for (int i = 1; i <= processor.Document.Pages.Count; i++)
+                {
 
                     // Export pages to bitmaps.
-                    Bitmap image = processor.CreateBitmap(i, largestEdgeLength);
+                    DXImage image = processor.CreateDXBitmap(i, largestEdgeLength);
 
                     // Save the bitmaps.
-                    image.Save("..\\..\\MyBitmap" + i + ".bmp");
+                    image.Save("..\\..\\MyBitmap" + i + ".bmp", DXImageFormat.Bmp);
                 }
+
+                PdfPageRenderingParameters renderingParameters = PdfPageRenderingParameters.CreateWithResolution(72f);
+                // Export pages to SVGs
+                DXImage svgImage = processor.CreateSvgImage(pageNumbers[0], renderingParameters);
+
+                // Save the images
+                svgImage.Save("..\\..\\MySvg", DXImageFormat.Svg);
             }
         }
     }
 }
+
 
 
--- VB/ExportToImage/Program.vb
+++ VB/ExportToImage/Program.vb
@@ -1,26 +1,41 @@
+﻿Imports DevExpress.Drawing
 Imports DevExpress.Pdf
-Imports System.Drawing
 
 Namespace ExportToImage
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
 
-    Friend Class Program
+			Dim largestEdgeLength As Integer = 1000
+			Dim pageNumbers() As Integer = { 1, 3, 5 }
 
-        Shared Sub Main(ByVal args As String())
-            Dim largestEdgeLength As Integer = 1000
-            Dim pageNumbers As Integer() = New Integer() {1, 3, 5}
-            ' Create a PDF Document Processor.
-            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
-                ' Load a document. 
-                processor.LoadDocument("..\..\Document.pdf")
-                ' Export pages to a multi-page tiff image.
-                processor.CreateTiff("..\..\Image.tiff", largestEdgeLength, pageNumbers)
-                For i As Integer = 1 To processor.Document.Pages.Count
-                    ' Export pages to bitmaps.
-                    Dim image As Bitmap = processor.CreateBitmap(i, largestEdgeLength)
-                    ' Save the bitmaps.
-                    image.Save("..\..\MyBitmap" & i & ".bmp")
-                Next
-            End Using
-        End Sub
-    End Class
+			' Create a PDF Document Processor.
+			Using processor As New PdfDocumentProcessor()
+
+				' Load a document. 
+				processor.LoadDocument("..\..\Document.pdf")
+
+				' Export pages to a multi-page tiff image.
+				processor.CreateTiff("..\..\Image.tiff", pageNumbers, 96)
+
+				For i As Integer = 1 To processor.Document.Pages.Count
+
+					' Export pages to bitmaps.
+					Dim image As DXImage = processor.CreateDXBitmap(i, largestEdgeLength)
+
+					' Save the bitmaps.
+					image.Save("..\..\MyBitmap" & i & ".bmp", DXImageFormat.Bmp)
+				Next i
+
+				Dim renderingParameters As PdfPageRenderingParameters = PdfPageRenderingParameters.CreateWithResolution(72F)
+				' Export pages to SVGs
+				Dim svgImage As DXImage = processor.CreateSvgImage(pageNumbers(0), renderingParameters)
+
+				' Save the images
+				svgImage.Save("..\..\MySvg", DXImageFormat.Svg)
+			End Using
+		End Sub
+	End Class
 End Namespace
+
+
+
